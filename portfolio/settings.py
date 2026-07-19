53b4f9261e8e5b90a1826ff9772f1ff697d22a35
Your Name <you@example.com>
2026-07-19 16:30:32 +0530

    feat: initialize Django settings with production security and email configurations

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["arjav-dosi.onrender.com"]
+ALLOWED_HOSTS = [
+    "arjav-dosi.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
